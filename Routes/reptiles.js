03b21c49c7c65523d209b9814f3d7e52816ac5d1
Moon2024-light <brite_moon2008@yahoo.com>
2025-02-15 22:50:15 +0100

    removed //

--- Routes/reptiles.js
+++ Routes/reptiles.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const reptiles = require('../data/reptiles'); // Importing reptile data from a separate file
+const reptiles = require('../data/reptiles'); 
 
-// Route to get all reptiles
+
 router.get('/', (req, res) => {
     res.render('group-page', { title: "Reptiles", animals: reptiles });
 });
 
-// Route for individual reptiles
+
 router.get('/:name', (req, res) => {
     const reptile = reptiles.find(r => r.name.toLowerCase().replace(/\s+/g, '') === req.params.name.toLowerCase());
     
